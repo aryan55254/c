4708a4c2c58cf4b837ba70446e6c79b5192ec4c4
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-01-31 10:55:04 +0530

    huh

--- 01/0.6.c
+++ 01/0.6.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>//for using booleans
 int main(){
     int x = 5;
     int y = 8;
@@ -10,3 +11,4 @@ int main(){
 }
 //implicit done by compiler automatically like when u assign int value to a float identifier it convt to floata utomatically 
 //explicit is one in which we have to use certain stuff like ()
+//booleans return as 1/0 so use %d
